77eff54a322b744fad82f7ef3576b8a864050802
Alexis Degraeve <alexis.degraeve@hotmail.com>
2023-02-17 15:09:20 +0100

    css open door

--- chatbotineg/src/app/app-routing.module.ts
+++ chatbotineg/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './escape-game/about/about.component';
 import { GameComponent } from './escape-game/game/game.component';
+import { Enigme1Component } from './escape-game/game/room1/enigme1/enigme1.component';
 import { WelcomeComponent } from './escape-game/welcome/welcome.component';
 
 const routes: Routes = [
@@ -9,6 +10,7 @@ const routes: Routes = [
   { path: 'welcome', component: WelcomeComponent },
   { path: 'game', component: GameComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'enigme1', component: Enigme1Component },
 ];
 
 @NgModule({
