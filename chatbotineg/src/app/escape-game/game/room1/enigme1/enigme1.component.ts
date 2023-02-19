92ab6046d17abb3db6b104109327266c83faea0c
Alexis Degraeve <alexis.degraeve@hotmail.com>
2023-02-19 21:39:23 +0100

    play simon game

--- chatbotineg/src/app/escape-game/game/room1/enigme1/enigme1.component.ts
+++ chatbotineg/src/app/escape-game/game/room1/enigme1/enigme1.component.ts
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
 })
 export class Enigme1Component {
 
+
 }
 /* Rejouer la musique */
--- chatbotineg/src/app/escape-game/game/room1/enigme1/simon/simon.component.ts
+++ chatbotineg/src/app/escape-game/game/room1/enigme1/simon/simon.component.ts
@@ -3,8 +3,55 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-simon',
   templateUrl: './simon.component.html',
-  styleUrls: ['./simon.component.scss']
+  styleUrls: ['./simon.component.scss'],
 })
 export class SimonComponent {
+  public animateBox1 = false;
+  public animateBox2 = false;
+  public animateBox3 = false;
+  public animateBox4 = false;
 
+  playAudio() {
+    this.resetAnimateBox();
+    let nb = this.getRandomInt(4) + 1;
+    let audio = new Audio();
+    console.log(nb);
+    audio.src = `/assets/sound/simonSound${nb}.mp3`;
+    audio.load();
+    audio.play();
+
+    switch (nb) {
+      case 1:
+        this.animateBox1 = true;
+        break;
+      case 2:
+        this.animateBox2 = true;
+        break;
+      case 3:
+        this.animateBox3 = true;
+        break;
+      case 4:
+        this.animateBox4 = true;
+        break;
+      default:
+        break;
+    }
+
+    // setTimeout(() => {
+    //   audio.src = "/assets/sound/simonSound2.mp3";
+    //   audio.load();
+    //   audio.play();
+    // }, 1500);
+  }
+
+  resetAnimateBox() {
+    this.animateBox1 = false;
+    this.animateBox2 = false;
+    this.animateBox3 = false;
+    this.animateBox4 = false;
+  }
+
+  getRandomInt(max: number) {
+    return Math.floor(Math.random() * max);
+  }
 }
