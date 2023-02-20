5d5217641ecc64ca1517e28431cc777733319326
Alexis Degraeve <alexis.degraeve@hotmail.com>
2023-02-20 21:27:46 +0100

    random number enigme 2

--- chatbotineg/src/app/escape-game/game/room1/enigme2/enigme2.component.ts
+++ chatbotineg/src/app/escape-game/game/room1/enigme2/enigme2.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class Enigme2Component {
 
+  nbX = 0;
+  nbY = 0;
+  try = 8;
+
+  startGame() {
+    this.try = 8;
+    this.nbX = this.getRandomInt(25);
+    this.nbY = this.getRandomInt(15);
+    console.log(this.nbX);
+    console.log(this.nbY);
+  }
+
+  checkFace(i:number, j:number) {
+    if ((i == this.nbX) && (j === this.nbY)) {
+      console.log('GAGNE');
+      this.startGame();
+    } else {
+      if(this.try === 0 ) {
+        console.log('PERDU');
+        this.startGame();
+      } else {
+        console.log('RECOMMENCE');
+        this.try--;
+      }
+    }
+  }
+
+  getRandomInt(max: number) {
+    return Math.floor(Math.random() * max);
+  }
 }
