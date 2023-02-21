946a8feeec4f39d30778b7dcbd2e56262d58a038
Alexis Degraeve <alexis.degraeve@hotmail.com>
2023-02-21 19:25:51 +0100

    add mountain

--- chatbotineg/src/app/escape-game/game/room1/enigme1/enigme1.component.ts
+++ chatbotineg/src/app/escape-game/game/room1/enigme1/enigme1.component.ts
@@ -1,23 +1,28 @@
+import { trigger, transition, style, animate } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-enigme1',
   templateUrl: './enigme1.component.html',
-  styleUrls: ['./enigme1.component.scss']
+  styleUrls: ['./enigme1.component.scss'],
+  animations: [
+    trigger(
+      'enterAnimation', [
+        transition(':leave', [
+          style({transform: 'translateX(0)', opacity: 1}),
+          animate('500ms', style({transform: 'translateX(-100%)', opacity: 0}))
+        ])
+      ]
+    )
+  ],
 })
 export class Enigme1Component {
-  rotateWheel = false;
-  playSimon = false;
+  showSimon = false;
+  mountain = true;
 
-  spinWheel() {
-    if(!this.playSimon) {
-      this.rotateWheel =true;
-      setTimeout(() => {
-        this.playSimon = true;
-        this.rotateWheel = false;
-      }, 1000);
-    }
-
+  moveMountain() {
+    this.mountain = false;
+    this.showSimon = true;
   }
 
 }
--- chatbotineg/src/app/escape-game/game/room1/enigme1/simon/simon.component.ts
+++ chatbotineg/src/app/escape-game/game/room1/enigme1/simon/simon.component.ts
@@ -1,12 +1,28 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-simon',
   templateUrl: './simon.component.html',
   styleUrls: ['./simon.component.scss'],
+  animations: [
+    trigger(
+      'enterAnimation', [
+        transition(':enter', [
+          style({transform: 'translateY(-10%)', opacity: 0}),
+          animate('500ms', style({transform: 'translateY(0)', opacity: 1}))
+        ]),
+        transition(':leave', [
+          style({transform: 'translateY(0)', opacity: 1}),
+          animate('500ms', style({transform: 'translateY(-10%)', opacity: 0}))
+        ])
+      ]
+    )
+  ],
 })
 export class SimonComponent {
-  @Input() showplay = false;
+  @Input()showSimon = false;
+  @Input()showplay = true;
   public animateBox1 = false;
   public animateBox2 = false;
   public animateBox3 = false;
