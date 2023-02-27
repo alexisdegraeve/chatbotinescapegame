dff4dc444ef033067e5d27a2b17adfb687e389a6
Alexis Degraeve <alexis.degraeve@hotmail.com>
2023-02-27 20:55:14 +0100

    add array barque

--- chatbotineg/src/app/escape-game/game/room2/enigme6/enigme6.component.ts
+++ chatbotineg/src/app/escape-game/game/room2/enigme6/enigme6.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GameService } from '../../game.service';
 
 @Component({
@@ -6,41 +6,67 @@ import { GameService } from '../../game.service';
   templateUrl: './enigme6.component.html',
   styleUrls: ['./enigme6.component.scss']
 })
-export class Enigme6Component {
+export class Enigme6Component implements OnInit{
   rive_droite = false;
-  mario = false;
+  mario = true;
   mushroom = false;
   bowser = false;
+  maxbarque = 2;
+  barques = [0];
 
   constructor(private gameService: GameService) {
     this.gameService.showInformationBar = true;
     this.gameService.roomNumber = 2;
+    this.barques.length = 2;
+  }
+  ngOnInit(): void {
+
   }
 
   traverser() {
     this.rive_droite = !this.rive_droite;
+    console.log(this.barques);
   }
   selectMario(){
-    this.mario= !this.mario;
+    if(this.barques.length <= this.maxbarque) {
+      this.mario= !this.mario;
+      this.barques.push(0);
+    }
   }
 
   selectMushroom() {
-    this.mushroom = !this.mushroom;
+    if(this.barques.length <= this.maxbarque) {
+      this.mushroom = !this.mushroom;
+      this.barques.push(1);
+    }
   }
 
   selectBowser(){
-    this.bowser = !this.bowser;
+    if(this.barques.length <= this.maxbarque) {
+      this.bowser = !this.bowser;
+      this.barques.push(2);
+    }
   }
 
   unselectMario(){
     this.mario= false;
+    this.removeBarques(0);
   }
 
   unselectMushroom() {
     this.mushroom = false;
+    this.removeBarques(1);
   }
 
   unselectBowser(){
     this.bowser = false;
+    this.removeBarques(2);
+  }
+
+  removeBarques(searchIndex: number) {
+    const index = this.barques.indexOf(searchIndex);
+    if (index > -1) {
+      this.barques.splice(index, 1);
+    }
   }
 }
