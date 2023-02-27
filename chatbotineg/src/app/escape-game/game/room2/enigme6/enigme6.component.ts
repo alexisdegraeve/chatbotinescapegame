867dbefffb3addb9f2002e077accb1b8730c8ed3
Alexis Degraeve <alexis.degraeve@hotmail.com>
2023-02-27 20:19:58 +0100

    move caracters barque

--- chatbotineg/src/app/escape-game/game/room2/enigme6/enigme6.component.ts
+++ chatbotineg/src/app/escape-game/game/room2/enigme6/enigme6.component.ts
@@ -7,8 +7,40 @@ import { GameService } from '../../game.service';
   styleUrls: ['./enigme6.component.scss']
 })
 export class Enigme6Component {
+  rive_droite = false;
+  mario = false;
+  mushroom = false;
+  bowser = false;
+
   constructor(private gameService: GameService) {
     this.gameService.showInformationBar = true;
     this.gameService.roomNumber = 2;
+  }
+
+  traverser() {
+    this.rive_droite = !this.rive_droite;
+  }
+  selectMario(){
+    this.mario= !this.mario;
+  }
+
+  selectMushroom() {
+    this.mushroom = !this.mushroom;
+  }
+
+  selectBowser(){
+    this.bowser = !this.bowser;
+  }
+
+  unselectMario(){
+    this.mario= false;
+  }
+
+  unselectMushroom() {
+    this.mushroom = false;
+  }
+
+  unselectBowser(){
+    this.bowser = false;
   }
 }
