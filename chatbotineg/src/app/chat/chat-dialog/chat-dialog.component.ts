98589b09c5defa9b2dbc0a14358b5d5a0248d8d9
Alexis Degraeve <alexis.degraeve@hotmail.com>
2023-03-12 16:15:19 +0100

    switch language chatbot

--- chatbotineg/src/app/chat/chat-dialog/chat-dialog.component.ts
+++ chatbotineg/src/app/chat/chat-dialog/chat-dialog.component.ts
@@ -1,5 +1,7 @@
 import { animate, style, transition, trigger } from '@angular/animations';
-import { AfterViewInit, Component, EventEmitter, Input, Output, ViewChild, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Input, Output, Sanitizer, ViewChild, ViewEncapsulation } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-chat-dialog',
@@ -23,4 +25,14 @@ import { AfterViewInit, Component, EventEmitter, Input, Output, ViewChild, ViewE
 })
 export class ChatDialogComponent {
   @Input() show = false;
+  public  urlFrench = this.sanitizer.bypassSecurityTrustResourceUrl('https://console.dialogflow.com/api-client/demo/embedded/1e88023f-dfcc-4b80-afc0-4982069fed26');
+  public urlEnglish = this.sanitizer.bypassSecurityTrustResourceUrl('https://console.dialogflow.com/api-client/demo/embedded/8bfefc6f-e7c2-49e9-bd50-fd73e1e1c0eb');
+
+  constructor(private translate: TranslateService, private sanitizer: DomSanitizer) {
+
+  }
+
+  get currentLang() {
+    return this.translate.currentLang;
+  }
 }
