4379a03bef2c1e13bf9c1fd1c84a00c95ae510d3
Alexis Degraeve <alexis.degraeve@hotmail.com>
2023-03-12 18:25:44 +0100

    add sondage english

--- chatbotineg/src/app/escape-game/sondage/sondage.component.ts
+++ chatbotineg/src/app/escape-game/sondage/sondage.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-sondage',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./sondage.component.scss']
 })
 export class SondageComponent {
+  public urlFrench = this.sanitizer.bypassSecurityTrustResourceUrl('https://docs.google.com/forms/d/e/1FAIpQLScjHO6hu1QHxgqyaVgoAdPmYfID6J1rTeRE0dAJ-CFdJLAOXA/viewform?usp=pp_url');
+  public urlEnglish = this.sanitizer.bypassSecurityTrustResourceUrl('https://docs.google.com/forms/d/1NiMRWTqF29Cx8lWlubbXGy6JhvRJFDHmR4frKC8Lfx0/prefill');
+
+  constructor(private translate: TranslateService, private sanitizer: DomSanitizer) {
+
+  }
+  get currentLang() {
+    return this.translate.currentLang;
+  }
 
 }
